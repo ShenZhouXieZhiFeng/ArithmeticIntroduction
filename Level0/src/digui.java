f22e3f00bff0f67bcaea2f2af62c294dad686ad7
Stone <827443469@qq.com>
2017-11-07 22:17:49 +0800

    复习递归，练习两种方式实现二分查找

--- Level0/src/digui.java
+++ Level0/src/digui.java
@@ -1,11 +1,41 @@
 
 public class digui {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println(que2(1729));
+//	/**
+//	 * @param args
+//	 */
+//	public static void main(String[] args) {
+//		que3("123",0);
+//	}
+	
+	//123 231 321 213 312 132 错误
+	public static void que3(String str,int n) {
+//		System.out.println(str);
+		if (n > 2) {
+			return;
+		}
+		System.out.println(str);
+		char[] chs = str.toCharArray();
+		for (int i = 0; i < chs.length; i++) {
+			char t1 = chs[n];
+			chs[n] = chs[i];
+			chs[i] = t1;
+			que3(new String(chs),n + 1);
+			char t2 = chs[n];
+			chs[n] = chs[i];
+			chs[i] = t2;
+		}
+//		if(n == 0){
+//			System.out.println(str);
+//		}
+//		char[] chs = str.toCharArray();
+//		char t = chs[n];
+//		chs[n] = chs[n+1];
+//		chs[n+1] = t;
+//		str = new String(chs);
+//		System.out.println(str);
+//		n++;
+//		que3(str,n);
 	}
 	
 	//写一个递归函数DigitSum(n)，输入一个非负整数，返回组成它的数字之和，
@@ -56,18 +86,34 @@ public class digui {
 	}
 	
 	//二分查找
-	public static int  rank(int key,int[] a) {
-		return rank(key,a,0,a.length - 1);
-	}
 	public static int rank(int key,int[] a,int lo,int hi) {
-		if(lo > hi) return -1;
-		int mid = lo + (hi - lo);
-		if(key < a[mid]) 
-			return rank(key, a,lo,mid - 1);
-		else if(key > a[mid])
-			return rank(key, a,mid + 1,hi);
-		else 
+		if(lo < hi)
+			return -1;
+		int mid = lo + (hi - lo)/2;
+		if (key < a[mid]) {
+			return rank(key, a, lo, mid - 1);
+		}else if (key > a[mid]) {
+			return rank(key, a, mid + 1, hi);
+		}else {
 			return mid;
+		}
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
