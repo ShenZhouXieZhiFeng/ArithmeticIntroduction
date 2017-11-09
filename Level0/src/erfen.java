17989b6a8f761982af2a78675699d6ade88b0f73
Stone <827443469@qq.com>
2017-11-09 21:21:50 +0800

    修改忽略

--- Level0/src/erfen.java
+++ Level0/src/erfen.java
@@ -6,7 +6,7 @@ public class erfen {
 	 */
 //	public static void main(String[] args) { 
 //		// TODO Auto-generated method stub
-//		int[] arr = {1,2,3,4,5,6};
+//		int[] arr = {1,2,3,4,5,6}; 
 //		System.err.println(rank(4, arr));
 //		System.err.println(rank2(4, arr));
 //	}
