f8d2743723c898db86368c2fbfec4ca71d23132f
Stone <827443469@qq.com>
2017-12-07 13:07:41 +0800

    leetcode 2 easy

--- C#/LeetCode/LeetCode/LeetCode01.cs
+++ C#/LeetCode/LeetCode/LeetCode01.cs
@@ -41,10 +41,58 @@ namespace LeetCode
 
             //Console.Write(GetIntersectionNode(h1,h2).val);
 
-            int[] n1 = { 1 };
-            int[] n2 = { 1, 2 };
-            Intersection(n1, n2);
+            //int[] n1 = { 1 };
+            //int[] n2 = { 1, 2 };
+            //Intersection(n1, n2);
+            //int[] nums = { 1, 0, 1, 1 };
+            //FindMaxConsecutiveOnes(nums);
+
+            int[] p = { 1, 2 };
+            MaxProfit(p);
         }
+
+        #region 20171207
+
+        //Say you have an array for which the ith element is the price of a given stock on day i.
+        //If you were only permitted to complete at most one transaction(ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+        public static int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length == 0)
+                return 0;
+            int res = 0;
+            int curMin = prices[0];
+            for (int i = 1; i < prices.Length; i++) {
+                curMin = Math.Min(curMin, prices[i]);
+                res = Math.Max(res, prices[i] - curMin);
+            }
+            return res;
+        }
+
+        //Given a binary array, find the maximum number of consecutive 1s in this array.
+        public static int FindMaxConsecutiveOnes(int[] nums)
+        {
+            if (nums == null)
+                return 0;
+            int res = 0;
+            int t = 0;
+            foreach (int v in nums) {
+                if (v == 1)
+                {
+                    t++;
+                }
+                else {
+                    if (res < t)
+                        res = t;
+                    t = 0;
+                }
+            }
+            if (t > res) {
+                return t;
+            }
+            return res;
+        }
+
+        #endregion
 
         #region 20171206
 
