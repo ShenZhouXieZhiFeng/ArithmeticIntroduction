23077b36754f8bc549afca7b35c0d471a41370ec
Stone <827443469@qq.com>
2017-12-09 22:35:23 +0800

    create new project for introduction by c++

---
+++ IntroductionDesign/IntroductionDesign/IntroductionDesign/IntroductionDesign.cpp
@@ -0,0 +1,15 @@
+// IntroductionDesign.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << 1 << endl;
+
+	system("pause");
+    return 0;
+}
+
